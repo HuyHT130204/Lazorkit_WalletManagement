31455199cd5a10a44ee1da2c71e8251a7a0ac7c7
HuyHT13022004 <huyht1302@gmail.com>
2025-08-29 19:02:17 +0700

    fixed i18

--- my-wallet-ui/next-i18next.config.js
+++ my-wallet-ui/next-i18next.config.js
@@ -1,14 +1,18 @@
 /** @type {import('next-i18next').UserConfig} */
 module.exports = {
-  defaultNS: 'common',
-  fallbackLng: 'en',
-  localePath: './public/locales',
-  reloadOnPrerender: process.env.NODE_ENV === 'development',
-  react: { 
-    useSuspense: false 
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'vi'],
+    localeDetection: false,
   },
+  defaultNS: 'common',
+  ns: ['common'],
+  fallbackLng: 'en',
+  localePath: typeof window === 'undefined' ? require('path').resolve('./public/locales') : '/locales',
+  reloadOnPrerender: process.env.NODE_ENV === 'development',
+  react: { useSuspense: false },
   interpolation: {
     escapeValue: false,
   },
-  debug: false,
+  debug: process.env.NODE_ENV === 'development',
 }
